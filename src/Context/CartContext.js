d902e92b1be9d08173206845068769e54e05a2de
Gustavo Casavieja <gcasavieja@gmail.com>
2022-12-14 15:51:23 -0300

    cart finish LOCAL STORAGE

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -1,39 +1,120 @@
-import { createContext,  useReducer } from "react";
+import { createContext, useState, useEffect, useContext, useReducer } from "react";
 import { CartReducer } from "./CartReducer";
 
 
 
 export const CartContext = createContext(null)
+export const useCartContext = () => useContext(CartContext);
 
 const initialState = {
     cartList: [],
     isInCart: false, 
-    qty: 0
+    count: 0
 }
 
-export const CartContextProvider = ({children}) => {
+export const CartContextProvider = ({defaultValue = [], children}) => {
 
-    //const [cartList, setCartList] = useState([])
+    const [cartList, setCartList] = useState(
+        JSON.parse(localStorage.getItem('cartList')) || defaultValue);
+    
+    useEffect(() => {
+        localStorage.setItem('cartList',  JSON.stringify(cartList)); 
+      }, [cartList]);
+ 
     
     const [state, dispatch] = useReducer(CartReducer, initialState)
 
     function addToCart(item, count){
+
+        const updatedCart = [...state.cartList]
+
+        //me devuelve el item del carro si ya esta
+        const updatedItemIndex = updatedCart.findIndex(
+            element => element.id === item.id
+          );
+
+
+        //si no esta
+        if (updatedItemIndex < 0) {
+            //lo agregamos con spread o forma antigua
+            const newItem = { ...item, cantidad: count }
+            //setCartList([...cartList, newItem])
+            updatedCart.push(newItem);
+            //setCartList(cartList.concat(newItem))
+            //console.log(cartList)
+            setCartList(updatedCart);
+        } 
+        else {
+            const updatedItem = {
+                ...state.cartList[updatedItemIndex], cantidad:count
+              };
+                //updatedItem.cantidad = qty;
+              updatedCart.splice(updatedItemIndex, 1, updatedItem);
+
+            //setCartList([...cartList, updatedItem.cantidad = qty])
+            setCartList(updatedCart);
+
+        }
         dispatch({
             type: "ADD_TO_CART",
-            payload: { item, count },
+            payload: { cartList: updatedCart },
           });
-        
+        //return { ...state, cart: cartList };
     }
 
+
+    /*
+    function addToCart(item, qty){
+        
+        const updatedCart = [...cartList]
+        
+        //me devuelve el item del carro si ya esta
+        const updatedItemIndex = updatedCart.findIndex(
+            element => element.id === item.id
+          );
+        
+        //si no esta
+        if (updatedItemIndex < 0) {
+            //lo agregamos con spread
+            const newItem = { ...item, cantidad: qty }
+            updatedCart.push(newItem);
+        } 
+        else {
+            const updatedItem = {
+                ...cartList[updatedItemIndex], cantidad:qty
+              };
+
+              updatedCart.splice(updatedItemIndex, 1, updatedItem);
+            
+        }
+        setCartList(updatedCart);
+    }
+*/
+
     function removeItem(itemId){
+        //console.log("Removing product with id: " + productId);
+        const updatedCart = [...state.cartList];
+        const updatedItemIndex = updatedCart.findIndex(item => item.id === itemId);
+
+        const updatedItem = {
+            ...updatedCart[updatedItemIndex]
+         };
+        //si se elimina la cantidad
+        //updatedItem.quantity--;
+        //if (updatedItem.quantity <= 0) {
+        updatedCart.splice(updatedItemIndex, 1);
+        //} else {
+        //    updatedCart[updatedItemIndex] = updatedItem;
+        //}
         dispatch({
             type: "REMOVE_PRODUCT",
-            payload: { itemId },
+            payload: { updatedCart },
           });
     }
 
     function clear(){
         const updatedCart = [];
+        
         dispatch({
             type: "REMOVE_CART",
             payload: { updatedCart },
@@ -41,21 +122,30 @@ export const CartContextProvider = ({children}) => {
     }
 
     function estaEnCart(itemId){
-        
+        let esta = false;
+        const updatedCart = [...state.cartList]
+
+        //me devuelve el item del carro si ya esta
+        const updatedItemIndex = updatedCart.findIndex(
+            element => element.id === itemId
+          );
+
+        //si no esta
+        if (updatedItemIndex >= 0) {
+            esta = true;
+        } 
         
         dispatch({
             type: "IS_IN_CART",
-            payload: { itemId },
+            payload: { esta },
           });
     }
     
-    console.log("carrito: " + state.cartList.length);
+ 
     return (
         
         <CartContext.Provider value={{ 
             cartList: state.cartList, 
-            qty: state.qty,
-            isInCart:state.isInCart,
             addToCart }}>
             {children}
         </CartContext.Provider>
--- src/Context/CartReducer.js
+++ src/Context/CartReducer.js
@@ -1,85 +1,27 @@
 export const CartReducer = (state, action) => {
 
-    const updatedCart = [...state.cartList]
-    
-
     switch (action.type) {
         case "ADD_TO_CART":
-
-            //me devuelve el item del carro si ya esta
-            const updatedItemIndex = updatedCart.findIndex(
-                element => element.id === action.payload.item.id
-            );
-
-            //si no esta
-            if (updatedItemIndex < 0) {
-                //lo agregamos con spread o forma antigua
-                const newItem = {
-                    ...action.payload.item,
-                    cantidad: action.payload.count
-                }
-                //setCartList([...cartList, newItem])
-                updatedCart.push(newItem);
-                //setCartList(cartList.concat(newItem))
-                //console.log(cartList)
-            } else {
-                const updatedItem = {
-                    ...state.cartList[updatedItemIndex],
-                    cantidad: action.payload.count
-                };
-                //updatedItem.cantidad = qty;
-                updatedCart.splice(updatedItemIndex, 1, updatedItem);
-
-            }
-
+            
+            console.log(action.payload.cartList)
             return {
                 ...state,
-                cartList: updatedCart,
-                qty: state.qty + action.payload.count,
-                isInCart: state.isInCart
+                cartList: action.payload.cartList
             };
-
         case "REMOVE_PRODUCT":
-            //console.log("Removing product with id: " + productId);
-            const updatedItemIndex2 = updatedCart.findIndex(item => item.id === action.payload.itemId);
-
-            const updatedItem = {
-                ...updatedCart[updatedItemIndex2]
-            };
-            //si se elimina la cantidad
-            //updatedItem.quantity--;
-            //if (updatedItem.quantity <= 0) {
-            updatedCart.splice(updatedItemIndex2, 1);
-            //} else {
-            //    updatedCart[updatedItemIndex] = updatedItem;
-            //}
             return {
                 ...state,
-                cartList: updatedCart,
-                    isInCart: state.isInCart
+                cartList: [action.payload.updatedCart],
             };
         case "REMOVE_CART":
             return {
                 ...state,
-                cartList: action.payload.updatedCart,
-                    isInCart: state.isInCart
+                cartList: [action.payload.updatedCart],
             };
         case "IS_IN_CART":
-            let esta = false;
-
-            //me devuelve el item del carro si ya esta
-            const updatedItemIndex3 = updatedCart.findIndex(
-                element => element.id === action.payload.itemId
-            );
-
-            //si no esta
-            if (updatedItemIndex3 >= 0) {
-                esta = true;
-            }
             return {
                 ...state,
-                cartList: updatedCart,
-                isInCart: esta
+                isInCart: [action.payload.esta],
             };
         default:
             return state;
